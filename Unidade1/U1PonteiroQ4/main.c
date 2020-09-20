0d74a6e77e93c8551cffea79954c24bffb0e2ce7
cauanliima <cauanmarcelo@hotmail.com>
2020-09-19 21:51:43 -0300

    teste

--- Unidade1/U1PonteiroQ4/main.c
+++ Unidade1/U1PonteiroQ4/main.c
@@ -17,41 +17,41 @@ int main()
       int   *p4;
       int   *p5;
 
-      /* (a) */ //---------> 20
+      /* (a) *///---------> 20
       valor = 10;
       p1 = &valor;
       *p1 = 20;
       printf("%d \n", valor);
 
-      /* (b) */ //---------> 29.0
+      /* (b) *///---------> 29.0
       temp = 26.5;
       p2 = &temp;
       *p2 = 29.0;
       printf("%.1f \n", temp);
 
-      /* (c) */  //---------> P
+      /* (c) *///---------> P
       p3 = &nome[0];
       aux = *p3;
       printf("%c \n", aux);
 
-      /* (d) */
+      /* (d) *///---------> e
       p3 = &nome[4];
       aux = *p3;
       printf("%c \n", aux);
 
-      /* (e) */
+      /* (e) *///--------->P
       p3 = nome;
       printf("%c \n", *p3);
 
-      /* (f) */
+      /* (f) *///---------> e
       p3 = p3 + 4;
       printf("%c \n", *p3);
 
-      /* (g) */
+      /* (g) *///--------->t
       p3--;
       printf("%c \n", *p3);
 
-      /* (h) */
+      /* (h) *///--------->31
       vetor[0] = 31;
       vetor[1] = 45;
       vetor[2] = 27;
@@ -59,26 +59,26 @@ int main()
       idade = *p4;
       printf("%d \n", idade);
 
-      /* (i) */
+      /* (i) *///--------->45
       p5 = p4 + 1;
       idade = *p5;
       printf("%d \n", idade);
 
-      /* (j) */
+      /* (j) *///--------->27
       p4 = p5 + 1;
       idade = *p4;
       printf("%d \n", idade);
 
-      /* (l) */
+      /* (l) *///--------->31
       p4 = p4 - 2;
       idade = *p4;
       printf("%d \n", idade);
 
-      /* (m) */
+      /* (m) *///--------->45
       p5 = &vetor[2] - 1;
       printf("%d \n", *p5);
 
-      /* (n) */
+      /* (n) *///---------> 27
       p5++;
       printf("%d \n", *p5);
       return(0);
